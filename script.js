0d2a3addf799d46d2697a228389362b749726764
V-advani-coding <veeradvani@hotmail.com>
2021-09-15 13:28:43 +0400

    added today's login.
    Co-authored-by: Suman Banik <SumanBanikSB@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,9 @@
+var login = document.getElementById('login_btn');
+
+window.onclick = function(event) {
+    login.style.display = 'none';
+}
+
+login.onclick = function (event) {
+    login.style.display = 'block';
+}
