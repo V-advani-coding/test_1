26503f9351cd45acda83b6e8e4bfe6513d8cb1dd
V-advani-coding <veeradvani@hotmail.com>
2021-09-17 10:20:26 +0400

    Did file uploads
    Co-authored-by: Suman Banik <SumanBanikSB@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var login = document.getElementById('login_btn');
+// var login = document.getElementById('login_btn');
 
-window.onclick = function(event) {
-    login.style.display = 'none';
-}
+// window.onclick = function(event) {
+//     login.style.display = 'none';
+// }
 
-login.onclick = function (event) {
-    login.style.display = 'block';
-}
+// login.onclick = function (event) {
+//     login.style.display = 'block';
+// }
+
+function test() {
+    location.href = 'lon_in_page_1.html';
+};
